3cb83f9ef4fe6a3ba724de1ddd8d88b8d51672ae
IrynaSokolova-Quanandrya <80633345+IrynaSokolova-Quanandrya@users.noreply.github.com>
2023-11-10 17:28:24 +0000

    Deploying to gh-pages from @ IrynaSokolova-Quanandrya/team-work-23@0af6309a39b3d83099dd0f037109d3feaff586a6 🚀

---
+++ index.3b1cb5fd.js
@@ -0,0 +1,3 @@
+(()=>{let e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",()=>{let a="true"===e.getAttribute("aria-expanded");e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")})})();//# sourceMappingURL=index.3b1cb5fd.js.map
+
+//# sourceMappingURL=index.3b1cb5fd.js.map
